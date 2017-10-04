e1c51131a5df881b7e4483347df45308156a80fb
ericinthehaus <31998942+ericinthehaus@users.noreply.github.com>
2017-10-04 15:55:49 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    
+}
+
+function draw(){
+    
+    background(255);
+    fill(0,0,255);
+        
+    rect( 50,50,50,50);
+    
+    rect (150,50,50,50 );
+    
+    line (125, 80, 125, 125);
+    
+    ellipse (125, 170, 150, 50 );
+    
+    
+}
+
